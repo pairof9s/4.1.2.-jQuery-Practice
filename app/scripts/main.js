5e3f230ffb81ce5a51f8b589817541c0940290a9
David Nine <pairof9s@gmail.com>
2016-06-07 22:48:43 -0400

    evening edits

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,48 +1,37 @@
 (function($){
   'use strict';
 
-  $('dt').click(function(event){
-    var $clickedSection = $(this);
-    var $section = $('dd');
+  // $('dt').click(function(event){
+  //   var $clickedSection = $(this);
+  //   var $section = $('dd');
+  //
+  //   $sections.removeClass('active');
+  //   $clickedSection.next().addClass('active');
+  //
+  //   $('dd').not('.active').animate({
+  //     'height': 0
+  //   }, 500, function(){
+  //     $(this).hide();
+  //   });
+  //
+  //   $clickedSection.animate({
+  //     'height': 0
+  //   }, 500);
+  //
 
-    $sections.removeClass('active');
-    $clickedSection.next().addClass('active');
-
-    $('dd').not('.active').animate({
-      'height': 0
-    }, 500, function(){
-      $(this).hide();
-    });
-
-    $clickedSection.animate({
-      'height': 0
-    }, 500);
-
-
-    $('.nav-list > li h2').click(function(event){
-      event.preventDefault();
-
-      var $section = $(this).parent();
-      var $subNav = $('ul.sub-nav');
-
-      /* First set the active on the item clicked */
-      $('.active').removeClass('active');
-      $section.find('ul.sub-nav').addClass('active');
-
-      $subNav.not('active').slideUp();
-      $('.active').slideDown();
-    })
-
-  });
-
-
-
-
-
-
-
-
-
+    // $('.nav-list > li h2').click(function(event){
+    //   event.preventDefault();
+    //
+    //   var $section = $(this).parent();
+    //   var $subNav = $('ul.sub-nav');
+    //
+    //   /* First set the active on the item clicked */
+    //   $('.active').removeClass('active');
+    //   $section.find('ul.sub-nav').addClass('active');
+    //
+    //   $subNav.not('active').slideUp();
+    //   $('.active').slideDown();
+    // });
 
 
 
