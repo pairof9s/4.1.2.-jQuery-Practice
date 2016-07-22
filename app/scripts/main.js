6224691790ad59322b3c324fe9a17c1619ecdf19
David Nine <pairof9s@me.com>
2016-07-22 16:03:51 -0400

    final edits for assignment submit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,13 +11,13 @@
     }
   });
 
-$( "li.box-off" ).hover(function() {
-  $(this).css('background-color', "#A0C840");
-  $(this).css('background-color', "#6A6C71");
-});
-
-  // $(".box-off").mouseover(function(){
-  // 	$(this).css(backgroundColor: "#A0C840");
-  // });
+// $( "li.list-group-item" ).hover(function() {
+//   $(this).css('background-color', "#A0C840");
+//   $(this).css('background-color', "#6A6C71");
+// });
+//
+//   $(".list-group-item").mouseover(function(){
+//   	$(this).css(backgroundColor: "#A0C840");
+//   });
 
 }(jQuery));
